3d92dddb920e3c5de64a3e2b0c156432ea789b75
Roman Volyanskiy <rvolyanskiy@gmail.com>
2017-01-21 18:29:50 +0200

    only 3 errors

--- js/script.js
+++ js/script.js
@@ -1,107 +1,71 @@
 window.onload = function(){
 
 	let btnAdd = document.getElementById('add'),
-		// btnDel = document.getElementById('btn-del'),
 		archive = document.getElementById('archive'),
 		newTask = document.getElementById('newTask'),
 		fieldCounter = document.getElementById('counter'),
 		counterAll = document.getElementById('counter-all');
-		list = document.getElementById('newTask').childElementCount;
-		counterAll.innerHTML = list;
 		
 		archive.addEventListener("click", archiveTask, false); 
-		// btnDel.addEventListener("click", remove, false); 
 		newTask.addEventListener("click", checkBoxes, false);
-		document.getElementById('add').addEventListener('click', add);
-		// show();
-
+		btnAdd.addEventListener('click', add);
+		show();
 		
-function get_todos() {
-    var todos = new Array;
-    var todos_str = localStorage.getItem('todo');
-    if (todos_str !== null) {
-        todos = JSON.parse(todos_str); 
-    }
-    return todos;
-}
- 
-function add() {
-    var task = document.getElementById('input').value;
-    var todos = get_todos();
-    todos.push(task);
-    localStorage.setItem('todo', JSON.stringify(todos));
- 
-    show();
- 
-    return false;
-}
- 
-function remove() {
-    var id = this.getAttribute('id');
-    var todos = get_todos();
-    todos.splice(id, 1);
-    localStorage.setItem('todo', JSON.stringify(todos));
-     show();
-     return false;
-}
- 
-function show() {
-
-	// let inputText = document.getElementById('input');
-	// 	if (inputText.value === ""){
-	// 		// alert("Write a new task");
-	// 	}
-		// else{
-				let date = new Date();
-				let niceDate = date => date < 10 ? date = "0"+ date : date;
-				let alldate = niceDate(date.getHours())+ ':' + niceDate(date.getMinutes()) + ':'+ niceDate(date.getSeconds()) + '';
-			    var todos = get_todos();
-			
-			var html;
-			// работает
-		    for(var i=0; i<todos.length; i++) {
-		        html += '<li><input type="checkbox" name="task" value="task">' + ' ' + todos[i] + ' ' + alldate +'<button class="dell btn" id="' + i  + '">Delete</button></li>';
-		    };
-     	// 	html += '<li><input type="checkbox" name="task" value="task" class="remove" id="' + i  + '>' + ' ' + todos[i] + ' ' + alldate +'</li>';
-		    // };
-
-		    html += '</ul>';
-			    // for(var i=0; i<todos.length; i++) {
-
-			    //    var html ='<li><input type="checkbox" name="task" value="task">' + ' ' + todos[i] + ' ' + alldate +'<button class="remove" id="' + i  + '">x</button></li>';
-			    // };
-
-			    document.getElementById('newTask').innerHTML = html;
-			 
-			    var buttons = document.getElementsByClassName('dell');
-			    for (var i=0; i < buttons.length; i++) {
-			        buttons[i].addEventListener('click', remove);
-			    // };
+		function get_todos() {
+			var todos = new Array;
+			var todos_str = localStorage.getItem('todo');
+			if (todos_str !== null) {
+				todos = JSON.parse(todos_str); 
+			}
+			return todos;
 		}
-		list = document.getElementById('newTask').childElementCount;
-		counterAll.innerHTML = list;
-	}
- 
- // }
+		 
+		function add() {
+			var task = document.getElementById('input').value;
+			if (task === ""){
+					alert("Write a new task");
+				}
+			else {
+			var todos = get_todos();
+			todos.push(task);
+			localStorage.setItem('todo', JSON.stringify(todos));
+		 	show();
+		 	return false;
+		 }
+		}
+		 
+		function remove() {
+			var id = this.getAttribute('id');
+			var todos = get_todos();
+			todos.splice(id, 1);
+			localStorage.setItem('todo', JSON.stringify(todos));
+			 show();
+			 return false;
+		}
+		 
 
-////////////////////////////////////
-	// function createToDo (){
-	// 	let inputText = document.getElementById('input');
-	// 	if (inputText.value === ""){
-	// 		alert("Write a new task")
-	// 	}
-	// 	else {
-	// 		let date = new Date();
-	// 		let niceDate = date => date < 10 ? date = "0"+ date : date;
-	// 		let alldate = niceDate(date.getHours())+ ':' + niceDate(date.getMinutes()) + ':'+ niceDate(date.getSeconds()) + '';
 
-	// 		newTask.innerHTML += '<li><input type="checkbox" name="task" value="a4">' + ' ' + (inputText.value) + ' ' + alldate +'</li>';
-	// 		inputText.value='';
-	// 	}
+		function show() {
+			let date = new Date();
+			let niceDate = date => date < 10 ? date = "0" + date : date;
+			let alldate = niceDate(date.getHours()) + ':' + niceDate(date.getMinutes()) + ':' + niceDate(date.getSeconds()) + '';
+			var todos = get_todos();
 
-	// 	list = document.getElementById('newTask').childElementCount;
-	// 	counterAll.innerHTML = list;
-	// }
+			var html='<ul id="yes">';
+			for (var i = 0; i < todos.length; i++) {
+				html += '<li><input type="checkbox" name="task" value="task">' + ' ' + todos[i] + ' ' + alldate + '<button class="dell btn" id="' + i + '">Delete</button></li>';
+			};
+			html += '</ul>';
+			document.getElementById('newTask').innerHTML = html;
+
+			var buttons = document.getElementsByClassName('dell');
+			for (var i = 0; i < buttons.length; i++) {
+				buttons[i].addEventListener('click', remove);
+			}
+
+			list = document.getElementById('yes').childElementCount;
+			counterAll.innerHTML = list;
+		}
 
 
 
@@ -127,6 +91,7 @@ function show() {
 	}
 
 ////////////////////////////////////
+// рабочая
 	function archiveTask() {
 		let archiveNeed = document.getElementsByTagName('input');
 		let array = Array.prototype.slice.call(archiveNeed);
@@ -140,10 +105,12 @@ function show() {
 		list = document.getElementById('newTask').childElementCount;
 			counterAll.innerHTML = list;
 	}
-show();
 ///////////////////////////////
+show();
+
 
 }
+
 
 
 
