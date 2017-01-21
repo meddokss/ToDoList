ab46f7bfc044aaa892d5a02a8041103e644c7988
Roman Volyanskiy <rvolyanskiy@gmail.com>
2017-01-21 17:44:55 +0200

    localStorage

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 window.onload = function(){
 
-	let btnAdd = document.getElementById('btn-add'),
-		btnDel = document.getElementById('btn-del'),
+	let btnAdd = document.getElementById('add'),
+		// btnDel = document.getElementById('btn-del'),
 		archive = document.getElementById('archive'),
 		newTask = document.getElementById('newTask'),
 		fieldCounter = document.getElementById('counter'),
@@ -9,19 +9,109 @@ window.onload = function(){
 		list = document.getElementById('newTask').childElementCount;
 		counterAll.innerHTML = list;
 		
-
-	archive.addEventListener("click", archiveTask, false); 
-	btnDel.addEventListener("click", archiveTask, false); 
-	newTask.addEventListener("click", checkBoxes, false);
-	btnAdd.addEventListener("click", createToDo, false);
+		archive.addEventListener("click", archiveTask, false); 
+		// btnDel.addEventListener("click", remove, false); 
+		newTask.addEventListener("click", checkBoxes, false);
+		document.getElementById('add').addEventListener('click', add);
+		// show();
 
 		
+function get_todos() {
+    var todos = new Array;
+    var todos_str = localStorage.getItem('todo');
+    if (todos_str !== null) {
+        todos = JSON.parse(todos_str); 
+    }
+    return todos;
+}
+ 
+function add() {
+    var task = document.getElementById('input').value;
+    var todos = get_todos();
+    todos.push(task);
+    localStorage.setItem('todo', JSON.stringify(todos));
+ 
+    show();
+ 
+    return false;
+}
+ 
+function remove() {
+    var id = this.getAttribute('id');
+    var todos = get_todos();
+    todos.splice(id, 1);
+    localStorage.setItem('todo', JSON.stringify(todos));
+     show();
+     return false;
+}
+ 
+function show() {
+
+	// let inputText = document.getElementById('input');
+	// 	if (inputText.value === ""){
+	// 		// alert("Write a new task");
+	// 	}
+		// else{
+				let date = new Date();
+				let niceDate = date => date < 10 ? date = "0"+ date : date;
+				let alldate = niceDate(date.getHours())+ ':' + niceDate(date.getMinutes()) + ':'+ niceDate(date.getSeconds()) + '';
+			    var todos = get_todos();
+			
+			var html;
+			// работает
+		    for(var i=0; i<todos.length; i++) {
+		        html += '<li><input type="checkbox" name="task" value="task">' + ' ' + todos[i] + ' ' + alldate +'<button class="dell btn" id="' + i  + '">Delete</button></li>';
+		    };
+     	// 	html += '<li><input type="checkbox" name="task" value="task" class="remove" id="' + i  + '>' + ' ' + todos[i] + ' ' + alldate +'</li>';
+		    // };
+
+		    html += '</ul>';
+			    // for(var i=0; i<todos.length; i++) {
+
+			    //    var html ='<li><input type="checkbox" name="task" value="task">' + ' ' + todos[i] + ' ' + alldate +'<button class="remove" id="' + i  + '">x</button></li>';
+			    // };
+
+			    document.getElementById('newTask').innerHTML = html;
+			 
+			    var buttons = document.getElementsByClassName('dell');
+			    for (var i=0; i < buttons.length; i++) {
+			        buttons[i].addEventListener('click', remove);
+			    // };
+		}
+		list = document.getElementById('newTask').childElementCount;
+		counterAll.innerHTML = list;
+	}
+ 
+ // }
+
+////////////////////////////////////
+	// function createToDo (){
+	// 	let inputText = document.getElementById('input');
+	// 	if (inputText.value === ""){
+	// 		alert("Write a new task")
+	// 	}
+	// 	else {
+	// 		let date = new Date();
+	// 		let niceDate = date => date < 10 ? date = "0"+ date : date;
+	// 		let alldate = niceDate(date.getHours())+ ':' + niceDate(date.getMinutes()) + ':'+ niceDate(date.getSeconds()) + '';
+
+	// 		newTask.innerHTML += '<li><input type="checkbox" name="task" value="a4">' + ' ' + (inputText.value) + ' ' + alldate +'</li>';
+	// 		inputText.value='';
+	// 	}
+
+	// 	list = document.getElementById('newTask').childElementCount;
+	// 	counterAll.innerHTML = list;
+	// }
+
+
+
+////////////////////////////////////
 	function checkBoxes() {
 		var inputElems = document.getElementsByTagName("input"),
 		allTask = newTask.getElementsByTagName('li'),
 		count = 0;
 		for (var i=0; i<inputElems.length; i++) {
-			if (inputElems[i].type == "checkbox" && inputElems[i].checked == true){
+			if (inputElems[i].checked == true){
 				count++;
 				allTask[i].style.textDecoration = 'line-through';
 				allTask[i].style.color = 'grey';
@@ -36,24 +126,7 @@ window.onload = function(){
 		}
 	}
 
-	function createToDo (){
-		let inputText = document.getElementById('input');
-		if (inputText.value === ""){
-			alert("Write a new task")
-		}
-		else {
-			let date = new Date();
-			let niceDate = date => date < 10 ? date = "0"+ date : date;
-			let alldate = niceDate(date.getHours())+ ':' + niceDate(date.getMinutes()) + ':'+ niceDate(date.getSeconds()) + '';
-
-			newTask.innerHTML += '<li><input type="checkbox" name="task" value="a4">' + ' ' + (inputText.value) + ' ' + alldate +'</li>';
-			inputText.value='';
-		}
-
-		list = document.getElementById('newTask').childElementCount;
-		counterAll.innerHTML = list;
-	}
-
+////////////////////////////////////
 	function archiveTask() {
 		let archiveNeed = document.getElementsByTagName('input');
 		let array = Array.prototype.slice.call(archiveNeed);
@@ -67,8 +140,67 @@ window.onload = function(){
 		list = document.getElementById('newTask').childElementCount;
 			counterAll.innerHTML = list;
 	}
+show();
+///////////////////////////////
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
